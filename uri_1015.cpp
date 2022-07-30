a25d833979a5a6df0e2f7cd380036432bf5d5901
davioaa <77934526+davioaa@users.noreply.github.com>
2022-07-30 19:41:31 -0300

    Create uri_1015.cpp

---
+++ uri_1015.cpp
@@ -0,0 +1,16 @@
+//distancia entre dois pontos
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double x1, y1, x2, y2, formula;
+
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
+
+    formula = sqrt(double(pow(x2-x1,2)+pow(y2-y1,2)));
+
+    printf("%.4lf\n", formula);
+
+return 0;
+}
