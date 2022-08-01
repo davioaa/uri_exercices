9f9126307e59478d14703f59be0e28cb8396ab22
davioaa <77934526+davioaa@users.noreply.github.com>
2022-08-01 19:53:05 -0300

    Create uri_1016.cpp

---
+++ uri_1016.cpp
@@ -0,0 +1,18 @@
+//URI 1016
+
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){ 
+    int distancia, minutos;
+
+    scanf("%d", &distancia);
+
+    minutos = distancia * 2;
+
+    printf("%d minutos\n", minutos);
+    
+return 0;
+}
